fdcac03b51198c758cd69f04787ff29d054edaf4
AutentificaIC41M <81339393+AutentificaIC41M@users.noreply.github.com>
2021-03-24 20:09:55 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyCKVpp1o0Y1pI8qUn5bX97NVjVYRdFanwA",
+    authDomain: "galicia-12380.firebaseapp.com",
+    projectId: "galicia-12380",
+    storageBucket: "galicia-12380.appspot.com",
+    messagingSenderId: "957717819974",
+    appId: "1:957717819974:web:050a1e7da7e77fea365047",
+    measurementId: "G-5SDXKC2TYM"
+    });
